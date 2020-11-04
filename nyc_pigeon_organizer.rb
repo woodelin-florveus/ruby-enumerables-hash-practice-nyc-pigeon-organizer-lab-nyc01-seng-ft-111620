eb93e5acebae8883611f107d7339b299cb6f4647
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-04 20:04:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require "pry"
 
 
 def nyc_pigeon_organizer(data)
-data.each_with_object({}) do |key, value, final_arr|
+data.each_with_object({}) do |(key, value), final_arr|
   binding.pry
   final_arr
 end
