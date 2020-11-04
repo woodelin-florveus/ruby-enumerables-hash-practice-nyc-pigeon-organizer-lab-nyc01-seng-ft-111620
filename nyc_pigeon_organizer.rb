adb24544342420dcbcaa6ba602f430a539084f76
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-04 20:23:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,6 +13,7 @@ data.each_with_object({}) do |(key, value), final_arr|
       if !final_arr[name][key]
         !final_arr[name][key] = []
       end
+        final_arr[name][key].push(inner_key)
     end
   end
   final_arr
