c4b5aaedb5349f5b37adaf7bfc9227d3f3180fef
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-04 20:12:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,6 +6,8 @@ data.each_with_object({}) do |(key, value), final_arr|
   # binding.pry
   value.each do |inner_key, names|
     binding.pry
+    names.each do |name|
+    end
   end
   final_arr
 end
